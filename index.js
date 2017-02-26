6e52d9db006aae016241acbd480f37efb8fd9084
kai-oswald <kaos1910@gmail.com>
2017-02-26 20:24:02 +0100

    remove deprecated code

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
 var request = require('request');
-var ip = "http://192.168.2.103:4000";
 var rgb = require("./rgb");
+var express = require('express');
+var bodyParser = require('body-parser');
+
 function changeColor(color) {
     rgb.setColor(color.r, color.g, color.b);
 }
 
-function changeColor2(color) {
-    var options = {
-        uri: ip,
-        method: 'POST',
-        contentType: "application/json",
-        json: color
-    };
-    request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            logColorChange(color);
-        } else {
-            console.log("could not change color.");
-            console.log(error);
-        }
-    });
-}
-
-var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json()); // to support JSON-encoded bodies
+
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
     res.render("index.html");
 });
@@ -50,9 +36,6 @@ function rndColor() {
     var b = Math.round(Math.random() * 255);
     return new Color(r, g, b);
 }
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
 
 function Color(r, g, b) {
     this.r = r;
@@ -63,3 +46,7 @@ function Color(r, g, b) {
 function logColorChange(color) {
     console.log("change color to: rgb(" + color.r + ", " + color.g + ", " + color.b + ").");
 }
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
